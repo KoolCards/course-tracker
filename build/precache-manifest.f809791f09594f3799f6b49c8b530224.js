2b2d9e4d3f477e3fbfcfff72ba086dd70588b94b
koolcards <krissatya09@gmail.com>
2019-05-08 19:09:24 -0700

    update node gitignore

---
+++ build/precache-manifest.f809791f09594f3799f6b49c8b530224.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae88bcd1fcd57dd5c4ef8066b6ddfb5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4257292e1e11b254ae9f",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "a6dfeeeeeafde331440e",
+    "url": "/static/js/2.40c7663f.chunk.js"
+  },
+  {
+    "revision": "4257292e1e11b254ae9f",
+    "url": "/static/js/main.0706e36f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
